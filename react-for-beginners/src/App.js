c8db7bcb8b6616f192fa601dd37008c8f3f93208
LAPTOP-C047TOU8\졍 <doeob6@ajou.ac.kr>
2022-10-11 21:21:30 +0900

    complete cleanup

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,28 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
   useEffect(() => {
-    console.log("only once");
+    console.log("hi");
+    return () => console.log("bye");
   }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("search for", keyword);
-    }
-  }, [keyword]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
